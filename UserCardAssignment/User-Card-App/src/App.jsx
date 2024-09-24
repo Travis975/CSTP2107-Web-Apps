d2f28c65e2f69dde5bf8323f52c94f3081f88ce3
Travis Schellenberg <travis.schellenberg@gmail.com>
2024-09-24 11:23:38 -0700

    Set up project file structure

--- UserCardAssignment/User-Card-App/src/App.jsx
+++ UserCardAssignment/User-Card-App/src/App.jsx
@@ -1,34 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from'./components/Footer/Footer'
+import UserList from './components/UserList/UserList'
+import data from './assets/people.json'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userData, setUserData] = useState(data)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {/* {Header Component} */}
+      <Header />
+
+      {/* User List */}
+      <h1>Below is a list of our esteemed users!</h1>
+      <UserList userData={userData} />
+
+      {/* Footer Component */}
+      <Footer />
+    </div>
   )
 }
 
