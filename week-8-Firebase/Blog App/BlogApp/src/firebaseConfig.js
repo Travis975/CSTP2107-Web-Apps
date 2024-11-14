e4731cff1ac2e0cc4d4b677ee7e0f32a77993593
Travis Schellenberg <travis.schellenberg@gmail.com>
2024-11-13 17:38:41 -0800

    made environmental variables for firebase config

--- week-8-Firebase/Blog App/BlogApp/src/firebaseConfig.js
+++ week-8-Firebase/Blog App/BlogApp/src/firebaseConfig.js
@@ -1,21 +1,27 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD1cJSCrkqvaSONxIbGRaP9-e1czEQz4zE",
-  authDomain: "blogapp-a6e2b.firebaseapp.com",
-  projectId: "blogapp-a6e2b",
-  storageBucket: "blogapp-a6e2b.firebasestorage.app",
-  messagingSenderId: "697652056979",
-  appId: "1:697652056979:web:a3d6fccba7ea14c0eace66",
-  measurementId: "G-9EFL4F3LKQ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth();
+const db = getFirestore();
+
+export { auth, db };
