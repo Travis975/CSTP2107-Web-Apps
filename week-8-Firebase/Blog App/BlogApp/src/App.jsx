cf1a669072bcb4404f85a489a429bdbff5f69dc8
Travis Schellenberg <travis.schellenberg@gmail.com>
2024-11-13 18:01:06 -0800

    installed material UI packages

--- week-8-Firebase/Blog App/BlogApp/src/App.jsx
+++ week-8-Firebase/Blog App/BlogApp/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useRoutes } from 'react-router-dom';
+import SignInPage from './pages/Signin';
+import SignupPage from './pages/Signup';
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+import ViewBlogsPage from './pages/ViewBlogsPage';
+import ViewBlogDetailsPage from './pages/ViewBlogDetailsPage';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // React Router Setup
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const routes = useRoutes([
+    {
+      path: '/',
+      element: <SignInPage />
+    },
+    {
+      path: '/signup',
+      element: <SignupPage />
+    },
+    {
+      path: '/home',
+      element: <HomePage />
+    },
+    {
+      path: '/viewblogs',
+      element: <ViewBlogsPage />
+    },
+    {
+      path: '/viewblogs/:id',
+      element: <ViewBlogDetailsPage />
+    },
+    {
+      path: "*",
+      element: <NotFoundPage />
+    }
+
+  ])
+
+ return (
+  <>
+    <Navbar />
+    {routes}
+  </>
+ )
+
 }
 
 export default App
